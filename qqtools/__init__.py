2317dedde849e795817ac80b8ee783d6b12232b5
qq <qq@x1c.com>
2024-10-16 01:58:19 +0800

    v1.0.0

---
+++ qqtools/__init__.py
@@ -0,0 +1,11 @@
+# first-class class
+from .config.qyaml import dump_yaml, load_yaml
+from .qdict import qDict
+from .qtimer import Timer
+# first-class module
+from .torch import qcheckpoint, qdist, qscatter, qsparse
+# first-class funciton
+from .torch.qcheckpoint import recover, save_ckp
+from .torch.qgpu import parse_device
+from .torch.qoptim import CompositeScheduler, CompoundOptim
+from .torch.qscatter import scatter
