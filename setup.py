3853eb807efce1502bd5e4eb800e69336e3351aa
qq <qq@x1q.cc>
2025-02-24 15:21:41 +0800

    feat: batch assert

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 setuptools.setup(
     name="qqtools",
-    version="1.0.2",
+    version="1.0.3",
     author="qq",
     author_email="qq@x1q.cc",
     description="A small tool package for qq",
