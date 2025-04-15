a1fa01eff390a79f21193e6786db94d4f02a9b64
qq <qq@x1q.cc>
2025-04-16 00:03:48 +0800

    feat: qtimeformat

--- qqtools/config/qtime.py
+++ qqtools/config/qtime.py
@@ -12,5 +12,12 @@ def date_str():
     return datetime.datetime.now().strftime("%m%d")
 
 
-def hms_form(sec):
-    return time.strftime("%Hh %Mmin %Ss", time.gmtime(sec))
+def hms_form(sec, fullFormat=False):
+    if fullFormat:
+        return time.strftime("%Hh %Mmin %Ss", time.gmtime(sec))
+
+    t = time.gmtime(sec)
+    h = f"{t.tm_hour}h" if t.tm_hour else ""
+    m = f"{t.tm_min}min" if t.tm_min else ""
+    s = f"{t.tm_sec}s" if t.tm_sec or not (h or m) else ""
+    return " ".join(filter(None, [h, m, s]))
