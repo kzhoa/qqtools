a4df76dacf07f00ca5330fee89ebc1cb81da88b3
qq <qq@x1q.cc>
2025-09-24 18:22:44 +0800

    v1.1.11

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 setuptools.setup(
     name="qqtools",
-    version="1.1.10",
+    version="1.1.11",
     author="qq",
     author_email="qq@x1q.cc",
     description="A small tool package for qq",
