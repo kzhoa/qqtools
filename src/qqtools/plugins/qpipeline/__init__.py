02ee45c547893da8faef5f9c58dfb12ffd2c762b
qq <qq@x1q.cc>
2026-01-29 14:03:43 +0800

    docs: update docs

--- src/qqtools/plugins/qpipeline/__init__.py
+++ src/qqtools/plugins/qpipeline/__init__.py
@@ -1,5 +1,8 @@
 from .cmd_args import prepare_cmd_args
-from .entry_utils import get_param_stats, prepare_dataloder, prepare_loss, prepare_scheduler
+from .entry_utils import get_param_stats, prepare_dataloder
+from .entry_utils.loader import build_loader, build_loader_ddp, build_loader_trival
+from .entry_utils.loss import parse_comboloss_params, parse_loss_name, prepare_loss
+from .entry_utils.scheduler import prepare_scheduler
 from .middleware import middleware_extra_ckp_caches
 from .qpipeline import qPipeline
 from .task.qtask import PotentialTaskBase, qTaskBase
