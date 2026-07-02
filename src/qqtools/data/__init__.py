875b25a298f82331e86def2059e062ad34e969d8
qq <qq@x1q.cc>
2026-07-02 10:13:56 +0800

    Add balanced DDP samplers and qbalance helpers

--- src/qqtools/data/__init__.py
+++ src/qqtools/data/__init__.py
@@ -1,4 +1,5 @@
 from .list_utils import pad_sequences
+from .qbalance import compute_global_even_sort_order
 from .qbalanced_partition import qbalanced_partition
 from .qdatalist import qDataList
 from .qscaladict import qScalaDict
