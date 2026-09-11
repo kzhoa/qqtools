7999a3e6102b9db76721e969cf7b669297a82d1c
qq <qq@x1q.cc>
2026-09-11 13:49:12 +0800

    refactor: expose scheduling admission contract

--- src/qqtools/plugins/qexp/scheduling/__init__.py
+++ src/qqtools/plugins/qexp/scheduling/__init__.py
@@ -1,1 +1,5 @@
 """Pure scheduling decisions and domain projections."""
+from .admission import admits
+from .models import AdmissionInput, ResourceSnapshot, TaskDemand
+
+__all__ = ["AdmissionInput", "ResourceSnapshot", "TaskDemand", "admits"]
